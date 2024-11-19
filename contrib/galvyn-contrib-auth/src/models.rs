e7a5d1c126a554e15a68456ee45effe2f346b74a
myOmikron <git@omikron.dev>
2024-11-19 20:38:17 +0100

    Auth module

---
+++ contrib/galvyn-contrib-auth/src/models.rs
@@ -0,0 +1,12 @@
+use galvyn_core::re_exports::rorm::internal::field::Field;
+use galvyn_core::re_exports::rorm::Model;
+use webauthn_rs::prelude::Uuid;
+
+#[cfg(feature = "__local-user")]
+pub trait LocalUserAccess<U>
+where
+    U: Model,
+    <U as Model>::Primary: Field<Type = Uuid>,
+{
+    fn get_primary(&self) -> Uuid {}
+}
