63929f864f6513066d21f47a602e8879670c97c7
gammelalf <gammelalf@hopfen.space>
2025-03-06 13:31:33 +0100

    Re-export rorm for application author

--- galvyn/src/lib.rs
+++ galvyn/src/lib.rs
@@ -4,6 +4,20 @@ pub mod contrib {
     // pub use galvyn_contrib_tracing as tracing;
 }
 
+/// Re-export of [`rorm`](galvyn_core::re_exports::rorm)
+pub mod rorm {
+    pub use galvyn_core::re_exports::rorm::*;
+
+    /// Re-export from [`rorm`](galvyn_core::re_exports::rorm::DbEnum)
+    pub use galvyn_macros::DbEnum;
+
+    /// Re-export from [`rorm`](galvyn_core::re_exports::rorm::Model)
+    pub use galvyn_macros::Model;
+
+    /// Re-export from [`rorm`](galvyn_core::re_exports::rorm::Patch)
+    pub use galvyn_macros::Patch;
+}
+
 pub mod core {
     pub use galvyn_core::*;
 }
