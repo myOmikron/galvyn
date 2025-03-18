851e1eaa93395ed4a39b0120f495a2ba8c8d92f5
gammelalf <gammelalf@hopfen.space>
2025-03-18 21:58:58 +0100

    Fixed auth module

--- contrib/galvyn-contrib-auth/src/handler/local.rs
+++ contrib/galvyn-contrib-auth/src/handler/local.rs
@@ -2,7 +2,7 @@ use crate::models::{LocalAccount, WebAuthnKey};
 use crate::{AuthModule, MaybeAttestedPasskey};
 use galvyn_core::re_exports::axum::Json;
 use galvyn_core::session::Session;
-use galvyn_core::stuff::api_error::ApiResult;
+use galvyn_core::stuff::api_error::{ApiError, ApiResult};
 use galvyn_core::Module;
 use galvyn_macros::{delete, put};
 
@@ -13,7 +13,10 @@ pub async fn set_local_password(
     session: Session,
     Json(request): Json<SetLocalPasswordRequest>,
 ) -> ApiResult<()> {
-    let account_pk: i64 = session.get("account").await?.ok_or("Not logged-in")?;
+    let account_pk: i64 = session
+        .get("account")
+        .await?
+        .ok_or(ApiError::bad_request("Not logged-in"))?;
 
     let mut tx = AuthModule::global().db.start_transaction().await?;
 
@@ -21,7 +24,7 @@ pub async fn set_local_password(
         .condition(LocalAccount.account.equals(&account_pk))
         .optional()
         .await?
-        .ok_or("User is not a local one")?;
+        .ok_or(ApiError::bad_request("User is not a local one"))?;
 
     // TODO: hashing
 
@@ -37,7 +40,10 @@ pub async fn set_local_password(
 
 #[delete("/local/password", core_crate = "::galvyn_core")]
 pub async fn delete_local_password(session: Session) -> ApiResult<()> {
-    let account_pk: i64 = session.get("account").await?.ok_or("Not logged-in")?;
+    let account_pk: i64 = session
+        .get("account")
+        .await?
+        .ok_or(ApiError::bad_request("Not logged-in"))?;
 
     let mut tx = AuthModule::global().db.start_transaction().await?;
 
@@ -45,7 +51,7 @@ pub async fn delete_local_password(session: Session) -> ApiResult<()> {
         .condition(LocalAccount.account.equals(&account_pk))
         .optional()
         .await?
-        .ok_or("User is not a local one")?;
+        .ok_or(ApiError::bad_request("User is not a local one"))?;
 
     let has_webauthn = rorm::query(&mut tx, WebAuthnKey.key)
         .condition(WebAuthnKey.local_account.equals(&local_pk))
@@ -54,7 +60,7 @@ pub async fn delete_local_password(session: Session) -> ApiResult<()> {
         .into_iter()
         .any(|key| matches!(key.0, MaybeAttestedPasskey::Attested(_)));
     if !has_webauthn {
-        return Err("User has no other login method".into());
+        return Err(ApiError::bad_request("User has no other login method"));
     }
 
     rorm::update(&mut tx, LocalAccount)
