1181d2f36d35caa62a9db300f7bf50e3033d07c6
gammelalf <gammelalf@hopfen.space>
2025-08-28 16:26:37 +0200

    Implemented helper for propagating otel traces

--- galvyn/src/tracing/otel.rs
+++ galvyn/src/tracing/otel.rs
@@ -1,8 +1,16 @@
+use axum::extract::Request;
+use axum::http::{HeaderMap, HeaderName, HeaderValue};
+use axum::response::Response;
+use galvyn_core::middleware::SimpleGalvynMiddleware;
+use opentelemetry::propagation::{Extractor, Injector, TextMapPropagator};
 use opentelemetry::trace::{TraceError, TracerProvider};
-use opentelemetry::{Key, KeyValue, Value};
+use opentelemetry::{Context, Key, KeyValue, Value};
 use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::propagation::TraceContextPropagator;
 use opentelemetry_sdk::{runtime, trace, Resource};
-use tracing::Subscriber;
+use std::ops::ControlFlow;
+use tracing::{warn, Span, Subscriber};
+use tracing_opentelemetry::OpenTelemetrySpanExt;
 use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::Layer;
 
@@ -41,5 +49,69 @@ impl OpenTelemetrySetup {
             .with_threads(false) // It's a tokio worker anyway
             .with_tracked_inactivity(false)
             .with_tracer(tracer))
+    }
+}
+
+/// Checks incoming requests for opentelemetry headers and set the appropriate parent span.
+#[derive(Copy, Clone, Debug, Default)]
+pub struct ReceiveTracesMiddleware;
+impl SimpleGalvynMiddleware for ReceiveTracesMiddleware {
+    async fn pre_handler(&mut self, request: Request) -> ControlFlow<Response, Request> {
+        let context = headers_to_context(request.headers());
+        Span::current().set_parent(context);
+        ControlFlow::Continue(request)
+    }
+}
+
+/// Converts an opentelemetry context to a list of headers.
+///
+/// Those headers can be included in requests to preserve the opentelemetry trace.
+pub fn context_to_headers(context: &Context) -> HeaderMap {
+    let mut map = HeaderMap::new();
+    TraceContextPropagator::new().inject_context(context, &mut HeaderMapWrite(&mut map));
+    map
+}
+
+/// Reads an opentelemetry context from a list of headers.
+///
+/// Those headers can be received in requests to preserve the opentelemetry trace.
+pub fn headers_to_context(headers: &HeaderMap) -> Context {
+    TraceContextPropagator::new().extract(&HeaderMapRead(headers))
+}
+
+struct HeaderMapWrite<'a>(&'a mut HeaderMap);
+impl Injector for HeaderMapWrite<'_> {
+    fn set(&mut self, key: &str, value: String) {
+        let Ok(name) = HeaderName::try_from(key) else {
+            warn!(key, value, "Opentelemetry produced an invalid header");
+            return;
+        };
+        let Ok(value) = HeaderValue::try_from(&value) else {
+            warn!(key, value, "Opentelemetry produced an invalid header");
+            return;
+        };
+        self.0.insert(name, value);
+    }
+}
+
+struct HeaderMapRead<'a>(&'a HeaderMap);
+impl Extractor for HeaderMapRead<'_> {
+    fn get(&self, key: &str) -> Option<&str> {
+        let value = self.0.get(key)?;
+        value
+            .to_str()
+            .inspect_err(|_error| {
+                warn!(
+                    key,
+                    value = value.as_bytes(),
+                    "Received invalid opentelemetry header"
+                )
+            })
+            .ok()
+    }
+
+    fn keys(&self) -> Vec<&str> {
+        warn!("Extractor::keys is not implemented");
+        vec![]
     }
 }
