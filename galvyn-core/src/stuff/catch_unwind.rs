942f200c280d527b0f70965ab19d548eb0cde0a1
myOmikron <npfister@trufflepig-forensics.de>
2025-09-12 13:04:03 +0200

    Added catch unwind layer

---
+++ galvyn-core/src/stuff/catch_unwind.rs
@@ -0,0 +1,31 @@
+//! Middleware which catches stack unwinding cased by a panic
+//! and converts it into a `500` response and a logged error.
+
+use std::convert::Infallible;
+use std::panic::AssertUnwindSafe;
+
+use axum::extract::Request;
+use axum::response::IntoResponse;
+use axum::response::Response;
+use futures::FutureExt;
+
+use crate::middleware::AxumService;
+use crate::middleware::GalvynMiddleware;
+use crate::stuff::api_error::ApiError;
+
+/// Middleware which catches stack unwinding cased by a panic
+/// and converts it into a `500` response and a logged error.
+#[derive(Copy, Clone, Debug)]
+pub struct CatchUnwindLayer;
+impl GalvynMiddleware for CatchUnwindLayer {
+    async fn call<S: AxumService>(
+        self,
+        mut inner: S,
+        request: Request,
+    ) -> Result<Response, Infallible> {
+        match AssertUnwindSafe(inner.call(request)).catch_unwind().await {
+            Ok(response) => Ok(response.into_response()),
+            Err(_payload) => Ok(ApiError::server_error("Caught panic in handler").into_response()),
+        }
+    }
+}
