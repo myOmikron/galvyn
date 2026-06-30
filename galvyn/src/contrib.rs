bfc1081080d8da7be6ab9463936f8915377cfba3
gammelalf <gammelalf@hopfen.space>
2026-06-30 21:09:30 +0200

    Bundle contrib auth again

--- galvyn/src/contrib.rs
+++ galvyn/src/contrib.rs
@@ -2,8 +2,8 @@
 //!
 //! If this module is empty our you can't find a specific crate, then check galvyn's feature flags.
 
-//#[cfg(feature = "contrib-auth")]
-//pub use galvyn_contrib_auth as auth;
+#[cfg(feature = "contrib-auth")]
+pub use galvyn_contrib_auth as auth;
 //#[cfg(feature = "contrib-oauth")]
 //pub use galvyn_contrib_oauth as oauth;
 #[cfg(feature = "contrib-settings")]
